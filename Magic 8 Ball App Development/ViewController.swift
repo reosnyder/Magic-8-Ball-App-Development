9a517730a9ff1bf23861ed06da018d581c12ec2e
reosnyder <reo.snyder@students.dominican.edu>
2023-01-30 16:33:23 -0800

    Initial commit

---
+++ Magic 8 Ball App Development/ViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  Magic 8 Ball App Development
+//
+//  Created by Reo Snyder on 1/25/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+    @IBAction func buttonClicked(_ sender: Any) {
+    }
+    
+}
+
